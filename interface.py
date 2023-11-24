3879d6e33f1dc9b76125f1da99f540155e38d80a
Polof92 <briac.paul12@gmail.com>
2023-11-24 10:01:47 +0100

    Create interface.py

---
+++ interface.py
@@ -0,0 +1,7 @@
+import time
+bonjour = "Bonjour ! Vous trouverez juste en dessous toutes mes fonctionnalitées :"
+for i in range(len(bonjour)):
+    print(bonjour[i], end="")
+    time.sleep(0.25)
+print("Vous n'avez qu'à utiliser les chiffres pour choisir ce que vous voulez")
+
